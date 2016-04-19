3a7a30088ea5e46ede01518904e078535ae502ae
mastergant <dgant@icstars.org>
2016-04-19 14:12:45 +0000

    print out more

---
+++ about.js
@@ -0,0 +1,10 @@
+var name = "Dwan Gant";
+var age = "40";
+var idle = "Kobe Brynat, Payton Manning";
+var dislike = "lebron james, kobe haters, sometimes NBA 2k16";
+var love = "working on my lab, practicing my Node.js, creating my girl ah page for hairstyling" 
+
+console.log("My name is " + name + " my age is " + age);
+console.log("my idle is " + idle);
+console.log("What I dislike " + dislike);
+console.log("what I love doing" + love);
